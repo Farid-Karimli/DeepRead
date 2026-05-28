348ad8968fe3505fff1ccdf185c3bc980f2c8262
Farid Karimli <karimli.farid@icloud.com>
2026-05-28 15:51:00 -0400

    Typedefs for all components of paper interaction and agent operations

---
+++ backend/src/types.py
@@ -0,0 +1,94 @@
+from datetime import datetime
+from typing import List, Literal
+from pydantic import BaseModel
+
+#############################
+# Key Section Analysis and PaperMage Result
+# These are 1:1 with the paper
+#############################
+
+class CodeSnippet(BaseModel):
+    content: str
+    filepath: str
+    start_line: int
+    end_line: int
+
+class KeySectionsMappedSection(BaseModel):
+    section_id: str
+    section_header: str
+    code_snippets: list[CodeSnippet]
+
+class KeySectionsCodeResult(BaseModel):
+    paper_title: str
+    sections: list[KeySectionsMappedSection]
+
+class KeySectionsResult(BaseModel):
+    """
+        Result of the initial paper analysis. 
+        (1) Identify key sections, (2) Map key sections to code.
+    """
+    paper_title: str
+    github_repo_url: str
+    code_result: KeySectionsCodeResult
+
+# Same as papermage.magelib.box.Box
+class BoxModel(BaseModel):
+    page: int
+    l: float
+    t: float
+    w: float
+    h: float
+
+class SectionEntity(BaseModel):
+    entity_id: str
+    section_header: str
+    section_content: str
+    page_index: int
+    box: BoxModel
+
+class PaperMageResult(BaseModel):
+    paper_title: str
+    n_pages: int
+    sections: List[SectionEntity] # list of list of sections for each page
+
+
+######################
+# Ad-hoc User Selection Results
+# (content->code and code->content)
+# These get their own table as they can grow big for a paper and have 2 types. 
+######################
+
+class CodeToContentSection(BaseModel):
+    section_id: str
+    description: str
+
+
+class ContentToCodeResult(BaseModel):
+    code_snippet: CodeSnippet  # matches what we return today after rerank
+
+class CodeToContentResult(BaseModel):
+    sections: list[CodeToContentSection]  # section_id + description
+
+
+class ContentToCodeInputs(BaseModel):
+    content: str            # paper content selected by the user
+    repo_url: str
+    context: str            # surrounding context of the user's selection (auto-selected)
+
+class CodeToContentInputs(BaseModel):
+    code: str                 # code snippet selected by the user
+
+class PaperMappingRecord(BaseModel):
+    paper_id: str
+    mapping_type: Literal["content_to_code", "code_to_content"]
+    cache_key: str
+    inputs: ContentToCodeInputs | CodeToContentInputs
+    outputs: ContentToCodeResult | CodeToContentResult
+
+class PaperRecord(BaseModel):
+    id: str
+    paper_title: str
+    github_link: str
+    created_at: datetime
+    papermage_result: PaperMageResult | None = None
+    analysis_result: KeySectionsResult | None = None
