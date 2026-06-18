ac2e5412e2428fabb1d25b6e220b2b848d073f66
Farid Karimli <karimli.farid@icloud.com>
2026-06-18 15:18:55 -0400

    Add user context with username and id for now

---
+++ frontend/src/context/UserContext.tsx
@@ -0,0 +1,16 @@
+import { createContext } from 'react';
+
+export type User = {
+    username: string,
+    id: number,
+}
+
+type sidePanelContext = {
+    currentUser: User | null;
+    setUser: (user: User | null) => void;
+  };
+  
+export const UserContext = createContext<sidePanelContext>({
+    currentUser: null, //TODO: REMOVE DEFAULT VALUE
+    setUser: () => {},
+});
