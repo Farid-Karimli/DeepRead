a242b9973893ad7ca379e5bb95b7585277300b10
Farid Karimli <karimli.farid@icloud.com>
2026-06-29 14:30:40 -0400

    Comment out upload section

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -133,6 +133,7 @@ export default function Home({ handlePaperSubmit, onOpenCachedPaper, errorMessag
                         </button>
                     </form>
                 </div>
+                */}
             </section>
         </>
     );
